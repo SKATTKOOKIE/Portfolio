ff74a44885c902141697036a5d9bc53d9e2ebd5e
Brandon <118194245+SKATTKOOKIE@users.noreply.github.com>
2023-08-11 17:57:29 +0100

    Added collap div for university projects

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Navbar functionality
+
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
@@ -5,3 +7,25 @@ navBarToggle.addEventListener("click", function()
 {
   mainNav.classList.toggle("active");
 });
+
+// Toggle box functionality
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) 
+{
+  coll[i].addEventListener("click", function() 
+  {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") 
+    {
+      content.style.display = "none";
+    } 
+    else 
+    {
+      content.style.display = "block";
+    }
+  });
+}
